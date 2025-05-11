6d6c60c8d5091aefdb6de8c210771f11a0e0373b
ahmed-raza012003 <ahmedraza01272003@gmail.com>
2025-05-12 00:41:06 +0500

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -85,7 +85,7 @@ const About = () => {
             rel="noopener noreferrer"
             className="bg-green-500 py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
-Get in Touch Instantly via WhatsApp
+Ping Me on WhatsApp
 
 </a>
         </div>
